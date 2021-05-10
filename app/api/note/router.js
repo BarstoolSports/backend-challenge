a49287ee5b13c702306630341034e14172bf2707
Jeffrey Pope <jpopeufl@gmail.com>
2021-05-10 13:53:50 -0400

    forgot to save

--- app/api/note/router.js
+++ app/api/note/router.js
@@ -3,14 +3,14 @@ const auth = require('./auth')
 const validator = require('./validator')
 
 module.exports = (router) => {
-  router.get('/user/:id', async (req, res) => {
+  router.get('/user/:id/notes', async (req, res) => {
     await auth.requiresCurrentUser(req)
     await controller.read(req, res)
   })
 
-  router.put('/user/:id', async (req, res) => {
-    await auth.requiresCurrentUser(req)
-    await validator.update(req)
-    await controller.update(req, res)
+  router.post('/note', async (req, res) => {
+    await auth.requiresLogin(req)
+    await validator.create(req)
+    await controller.create(req, res)
   })
 }
--- app/api/note/validator.js
+++ app/api/note/validator.js
@@ -1,15 +1,14 @@
 const { validate, Validator } = require('app/api/common')
 const { body } = validate
 
-class UserValidator extends Validator {
-  async update(req) {
+class NoteValidator extends Validator {
+  async create(req) {
     const validations = [
-      body('email').optional().isEmail(),
-      body('firstName').optional().isLength(1, 64),
-      body('lastName').optional().isLength(1, 64)
+      body('title').notEmpty(),
+      body('message').notEmpty(),
     ]
     await this.validate(req, validations, { sanitize: 'query' })
   }
 }
 
-module.exports = new UserValidator()
+module.exports = new NoteValidator()
